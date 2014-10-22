bdf401bf88b840dbd4a48f5b8b0608f4ef2067c5
hyunahpark <phablue@gmail.com>
2014-10-22 14:10:04 -0500

    set basic socket client part

---
+++ public/javascripts/socket_client.js
@@ -0,0 +1,13 @@
+(function () {
+  var SocketClient = {
+    socket: io.connect();
+
+    getData: function () {
+    },
+
+    setData: function () {
+    }
+  };
+
+  window.SocketClient = SocketClient
+})();
