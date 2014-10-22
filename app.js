6651c5575f1c8de74a1b26bf8cec0c5fe9f2684c
hyunahpark <phablue@gmail.com>
2014-10-22 13:34:21 -0500

    set external file path

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+var path = require("path");
 var express = require("express");
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.Router());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function (req, res) {
   res.sendFile();
