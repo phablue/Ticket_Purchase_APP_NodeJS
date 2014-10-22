88b80add76feec6a0c5f432b0d0b96b4b4aa7a1c
hyunahpark <phablue@gmail.com>
2014-10-22 13:33:54 -0500

    set route

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
+app.use(express.Router());
+
+app.get("/", function (req, res) {
+  res.sendFile();
+});
+
+app.get("/seats", function (req, res) {
+  res.sendFile();
+});
+
 server.listen(3000, function () {
   console.log("Server Running at http://localhost:3000/");
 });
+
