ef3690cf1ac952eafeb0825ca21f2398b49e737c
hyunahpark <phablue@gmail.com>
2014-10-22 13:27:09 -0500

    execute web server

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
+server.listen(3000, function () {
+  console.log("Server Running at http://localhost:3000/");
+});
